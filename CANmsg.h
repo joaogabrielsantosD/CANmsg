be3b28a01f0d32e09e598e98c2ea11d7cf75e6ac
joaogabrielsantosD <joao.donato@ufpe.br>
2024-05-02 17:42:53 -0300

    .

--- CANmsg.h
+++ CANmsg.h
@@ -1,6 +1,64 @@
-#include "CANmsg.h"
+#ifndef CANMSG_H
+#define CANMSG_H
+
+#include "CAN/can_common.h"
+#include "CAN/esp32_can_builtin.h"
+#include "CAN/esp32_can.h"
 
 CAN_FRAME Tx_Rx_message;
+#define CAN CAN0
+#define MAX_MESSAGE_LENGTH 8
+typedef void (*callback)(CAN_FRAME*);
+
+class CANmsg
+{
+    private:
+        uint8_t _ext = 0;
+    protected:
+        CAN_FRAME msg;
+        int len = 0;
+        bool f = false;
+
+        void setup();
+    public:
+       CANmsg(gpio_num_t rx_id, gpio_num_t tx_id, uint32_t Baudrate);
+
+        ~CANmsg(); 
+
+        void init(callback isr); 
+
+        void init(callback isr, uint32_t Id);
+
+        void init(callback isr, uint32_t Id, uint32_t Mask);
+
+        int Set_Filter(uint32_t Id, uint32_t Mask, bool Extended);
+
+        void clear(uint32_t new_id, bool ext = false, uint8_t length = MAX_MESSAGE_LENGTH);
+
+        bool write();
+
+        bool SendMsgBuffer(uint32_t Id, bool ext, uint8_t length, unsigned char* data);
+
+        void Set_Debug_Mode(bool md);    
+
+        template<class T>
+        CANmsg &operator<<(const T value)
+        {
+            //if(len + sizeof(T) > 8) return NULL
+            memcpy(&msg.data.uint8[len], (uint8_t*)&value, sizeof(T));
+            len += sizeof(T);
+            return *this;
+        }
+
+        template<class Q>
+        CANmsg &operator=(Q& val)
+        {
+            val = *reinterpret_cast<Q*>(&msg.data.uint8[0]);
+            len -= sizeof(Q);
+            memcpy(msg.data.uint8, msg.data.uint8 + sizeof(Q), len);
+            return *this;
+        }   
+};
 
 CANmsg::CANmsg(gpio_num_t rx_id, gpio_num_t tx_id, uint32_t Baudrate) : msg(Tx_Rx_message)
 {
@@ -26,11 +84,6 @@ void CANmsg::init(callback isr)
 void CANmsg::init(callback isr, uint32_t Id)
 {
     this->init(isr, Id, 0);
-} 
-
-void CANmsg::init(callback isr)
-{
-    this->init(isr, 0, 0);
 } 
 
 void CANmsg::init(callback isr, uint32_t Id, uint32_t Mask)
@@ -90,3 +143,6 @@ void CANmsg::Set_Debug_Mode(bool md)
 {
     CAN.setDebuggingMode(md);
 }
+
+
+#endif
